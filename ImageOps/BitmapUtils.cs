f38286f35bb125ee7f52cb4c4c383b2b7246565d
Suremaker <wojciech.kotlarski@gmail.com>
2013-04-12 22:04:14 +0100

    + Exposed BitmapUtils and added method to fill bitmap by using color function

---
+++ ImageOps/BitmapUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageOps
+{
+	public static class BitmapUtils
+	{
+		public static unsafe Bitmap Create(int width, int height, Func<int, int, Color> colorFn)
+		{
+			var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+			BitmapData data = null;
+			try
+			{
+				data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+				var buf = (int*)data.Scan0.ToPointer();
+				for (int y = 0; y < height; ++y)
+					for (int x = 0; x < width; ++x)
+						*(buf++) = colorFn(x, y).ToArgb();
+			}
+			finally
+			{
+				if (data != null)
+					bmp.UnlockBits(data);
+			}
+			return bmp;
+		}
+
+		public static Bitmap Create(int width, int height, Color initialColor)
+		{
+			return Create(width, height, (x, y) => initialColor);
+		}
+
+		public static Bitmap Create(Color[,] pixels)
+		{
+			return Create(pixels.GetLength(1), pixels.GetLength(0), (x, y) => pixels[y, x]);
+		}
+	}
+}
--- ImageOps/BitmapWriter.cs
+++ ImageOps/BitmapWriter.cs
@@ -7,13 +7,18 @@ namespace ImageOps
 	{
 		public static unsafe Bitmap ToBitmap(this IPixelStream stream)
 		{
-			var bmp = new Bitmap(stream.ImageWidth, stream.ImageHeight);
-			var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-			var buffer = (uint*)data.Scan0.ToPointer();
-			foreach (var color in stream)
-				*(buffer++) = color.Argb;
-			bmp.UnlockBits(data);
-			return bmp;
+			using (stream)
+			{
+				var bmp = new Bitmap(stream.ImageWidth, stream.ImageHeight);
+				var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+				var buffer = (uint*)data.Scan0.ToPointer();
+
+				foreach (var color in stream)
+					*(buffer++) = color.Argb;
+
+				bmp.UnlockBits(data);
+				return bmp;
+			}
 		}
 	}
 }
