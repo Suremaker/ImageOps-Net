23f2b894f7966e4cc6e29d6c1f6681b0b7095d59
Suremaker <wojciech.kotlarski@gmail.com>
2014-09-06 19:09:57 +0100

    changed PixelRectangle to be class

--- ImageOps/PixelRectangle.cs
+++ ImageOps/PixelRectangle.cs
@@ -2,27 +2,31 @@
 
 namespace ImageOps
 {
-    public struct PixelRectangle
+    public class PixelRectangle
     {
+        private readonly int _x;
+        private readonly int _y;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly int _lastX;
+        private readonly int _lastY;
+
         public PixelRectangle(int x, int y, int width, int height)
-            : this()
         {
-            X = x;
-            Y = y;
-            Width = width;
-            Height = height;
+            _x = x;
+            _y = y;
+            _width = width;
+            _height = height;
+            _lastX = _x + _width - 1;
+            _lastY = _y + _height - 1;
         }
 
-        public int X { get; private set; }
-        public int Y { get; private set; }
-        public int Width { get; private set; }
-        public int Height { get; private set; }
-
-        public int Left { get { return X; } }
-        public int Right { get { return X + Width - 1; } }
-        public int Top { get { return Y; } }
-        public int Bottom { get { return Y + Height - 1; } }
-
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+        public int Width { get { return _width; } }
+        public int Height { get { return _height; } }
+        public int LastX { get { return _lastX; } }
+        public int LastY { get { return _lastY; } }
         public Point Location { get { return new Point(X, Y); } }
         public Size Size { get { return new Size(Width, Height); } }
 
@@ -38,7 +42,30 @@ namespace ImageOps
 
         public bool IsInside(int x, int y)
         {
-            return x >= Left && y >= Top && x <= Right && y <= Bottom;
+            return x >= _x && y >= _y && x <= LastX && y <= LastY;
+        }
+
+        public bool Equals(PixelRectangle other)
+        {
+            return _x == other._x && _y == other._y && _width == other._width && _height == other._height;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is PixelRectangle && Equals((PixelRectangle)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = _x;
+                hashCode = (hashCode * 397) ^ _y;
+                hashCode = (hashCode * 397) ^ _width;
+                hashCode = (hashCode * 397) ^ _height;
+                return hashCode;
+            }
         }
     }
 }
--- ImageOps/Sources/Regions/PolygonRegion.cs
+++ ImageOps/Sources/Regions/PolygonRegion.cs
@@ -6,40 +6,6 @@ namespace ImageOps.Sources.Regions
 {
     public class PolygonRegion : IRegion
     {
-        class Section
-        {
-            public Section(Point item1, Point item2)
-            {
-                P1 = item1;
-                P2 = item2;
-                if (P1.X != P2.X)
-                {
-                    A = (item1.Y - item2.Y) / (double)(item1.X - item2.X);
-                    B = item1.Y - A * item1.X;
-                }
-            }
-
-            public Point P1 { get; private set; }
-            public Point P2 { get; private set; }
-            public double A { get; private set; }
-            public double B { get; private set; }
-
-            public PointF? IsCrossedByLeftHorizontalRay(Point startPoint)
-            {
-                if (!IsOnOppositeSides(P1.Y, P2.Y, startPoint.Y))
-                    return null;
-                if (P1.X == P2.X)
-                    return P1.X <= startPoint.X ? (PointF?)new Point(P1.X, startPoint.Y) : null;
-                var crossX = (startPoint.Y - B) / A;
-                var isCrossed = !double.IsInfinity(crossX) && !double.IsNaN(crossX) && crossX <= startPoint.X && !(crossX == P1.X && startPoint.Y == P1.Y);
-                return isCrossed ? (PointF?)new PointF((float)crossX, startPoint.Y) : null;
-            }
-
-            private static bool IsOnOppositeSides(int sy1, int sy2, int py)
-            {
-                return Math.Sign(py - sy1) * Math.Sign(py - sy2) != 1;
-            }
-        }
         private readonly Section[] _sections;
 
         public PolygonRegion(params Point[] points)
